0a9b1ceb6d1a257077886eb39077d756ae0bfe95
CrownDaisy <1002886432@qq.com>
2018-06-04 21:17:25 +0800

    实现：英镑兑换人民币

--- example/pipelines.py
+++ example/pipelines.py
@@ -7,5 +7,17 @@
 
 
 class ExamplePipeline(object):
+
+    # 英镑兑换人民币汇率
+    exchange_rate = 8.5714
+
     def process_item(self, item, spider):
+        # 提取 item 的 price 字段 （如£53.74）
+        # 去掉前面英镑符号£，转换为float类型，乘以汇率
+        price = float(item['price'][1:]) * self.exchange_rate
+
+        # 保留2位小数，赋值回item的price字段
+        item['price']='￥%2f'%price
+        
         return item
+
